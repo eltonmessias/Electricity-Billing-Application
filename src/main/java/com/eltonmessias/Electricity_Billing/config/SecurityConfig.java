6d58d14b4dd4239434082de94e10fda97eda90df
elton <eltonmessias10@gmail.com>
2024-10-14 18:39:58 +0200

    Basic Authentication

--- src/main/java/com/eltonmessias/Electricity_Billing/config/SecurityConfig.java
+++ src/main/java/com/eltonmessias/Electricity_Billing/config/SecurityConfig.java
@@ -1,9 +1,42 @@
 package com.eltonmessias.Electricity_Billing.config;
 
+import com.eltonmessias.Electricity_Billing.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customer -> customer.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 }
