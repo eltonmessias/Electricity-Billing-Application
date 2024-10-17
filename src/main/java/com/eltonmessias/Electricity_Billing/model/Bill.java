28d023be9b7bf81c932695be62ababf54b417c02
elton <eltonmessias10@gmail.com>
2024-10-17 11:53:32 +0200

    feat: bill entity

---
+++ src/main/java/com/eltonmessias/Electricity_Billing/model/Bill.java
@@ -0,0 +1,26 @@
+package com.eltonmessias.Electricity_Billing.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @ManyToOne
+    @JoinColumn(name = "reading_id")
+    private Reading reading;
+    private double amount;
+    private LocalDateTime issuedDate;
+    private LocalDateTime dueDate;
+    private boolean paid = false;
+
+
+}
