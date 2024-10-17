7dfc8a304c0cdb5c55808b56779a42023052fa04
elton <eltonmessias10@gmail.com>
2024-10-17 09:38:01 +0200

    feat: get all readings

--- src/main/java/com/eltonmessias/Electricity_Billing/controller/ConsumptionController.java
+++ src/main/java/com/eltonmessias/Electricity_Billing/controller/ConsumptionController.java
@@ -3,10 +3,12 @@ package com.eltonmessias.Electricity_Billing.controller;
 import com.eltonmessias.Electricity_Billing.dto.ConsumptionDTO;
 import com.eltonmessias.Electricity_Billing.service.ConsumptionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +20,11 @@ public class ConsumptionController {
     @PostMapping("/consumptions")
     public String SaveConsumption(@RequestBody ConsumptionDTO consumptionDTO) {
         return consumptionService.registerConsumption(consumptionDTO);
+    }
+
+    @GetMapping("/readings")
+    public ResponseEntity<List<ConsumptionDTO>> getReadings() {
+        return new ResponseEntity<>(consumptionService.getAllConsumptions(), HttpStatus.OK);
     }
 
 }
