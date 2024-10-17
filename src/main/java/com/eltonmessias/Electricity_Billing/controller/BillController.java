7c6bbfb030acaa81747f53a4edba3924980be533
elton <eltonmessias1006@gmail.com>
2024-10-18 01:41:35 +0200

    feat: generate bill response

---
+++ src/main/java/com/eltonmessias/Electricity_Billing/controller/BillController.java
@@ -0,0 +1,22 @@
+package com.eltonmessias.Electricity_Billing.controller;
+
+import com.eltonmessias.Electricity_Billing.dto.BillDTO;
+import com.eltonmessias.Electricity_Billing.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BillController {
+
+    @Autowired
+    private BillService billService;
+
+    public ResponseEntity<BillDTO> generateBill(@PathVariable("id") long id) {
+        return new ResponseEntity<>(billService.createBill(id), HttpStatus.CREATED);
+    }
+}
